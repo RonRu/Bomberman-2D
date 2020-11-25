a803df8284ab0fa60e1d2669702ef54fbe06f282
Ron <ronru@edu.hac.ac.il>
2020-11-25 17:05:14 +0200

    Upload source code

---
+++ Bomberman/BonusAnimation.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Animation.h"
+class BonusAnimation : public Animation
+{
+public:
+
+	using::Animation::Animation; //using animation C-tor
+	virtual void play(float deltaTime, sf::RenderWindow& window) override; //play animation
+};
+
